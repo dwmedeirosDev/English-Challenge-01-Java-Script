73263e513943e48cc583a946e9ada5e3e6dcc2d5
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-28 11:05:45 -0300

    Done

---
+++ README.txt
@@ -0,0 +1,18 @@
+First English Homework Challenge
+
+1 - Create a structure that receives 3 arrays
+
+- Comedy
+- Action
+- Fantasy
+
+2 - Each Array must receive 3 objects
+
+- Title
+- Director
+- Year
+
+3 - Create a variable with a condition that displays the message in the console
+
+if: "My favorit movie is..."
+else: "Not even 5 stars..."
---
+++ script.js
@@ -0,0 +1,38 @@
+const movies = [
+    {
+        Comedy: [
+            {
+                Title: "Shaun of the Dead",
+                Director: "Edgar Wright",
+                Year: 2004
+            }
+        ],
+        Action: [
+            {
+                Title: "13 Hours: The Secret Soldiers of Benghazi",
+                Director: "Michael Bay",
+                Year: 2016
+            }
+        ],
+        Fantasy: [
+            {
+                Title: "Sucker Punch",
+                Director: "Zack Snyder",
+                Year: 2011
+            }
+        ]
+    }
+]
+
+console.log(movies)
+
+// ################################
+
+const favoriteMovie = "Titanic"
+
+if( favoriteMovie === "Titanic"){
+    console.log("My Favorite Movie is Titanic")   
+}else{
+    console.log("Not Even 5 Stars")
+}
+    
